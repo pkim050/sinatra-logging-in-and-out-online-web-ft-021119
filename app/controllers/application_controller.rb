229e7b73e01bf5a31fb34b4decb6dc014a3ec1a8
Patrick Kim <pkim050@gmail.com>
2019-04-06 15:35:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find(params[:username], params[:password])
+    session[:id] = @user.id
 
+    redirect '/account'
   end
 
   get '/account' do
@@ -24,4 +27,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
