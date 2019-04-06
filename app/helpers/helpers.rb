8d677c9e76d1ad4bdef9eda212af62e9bd39cfd9
Patrick Kim <pkim050@gmail.com>
2019-04-06 21:37:30 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,11 @@
 class Helpers
 
   def self.current_user(hash)
-    temp = self.find(hash.id)
+    temp = self.find(hash[:id])
     temp.username
   end
 
   def self.is_logged_in?(hash)
-
+    !!hash[:id]
   end
 end
