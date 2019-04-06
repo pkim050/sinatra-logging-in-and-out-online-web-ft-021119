fdecd2b329c22c4596b27ea5a36fd3324a8b9132
Patrick Kim <pkim050@gmail.com>
2019-04-06 21:46:01 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190406152622) do
+ActiveRecord::Schema.define(version: 2019_04_06_152622) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
